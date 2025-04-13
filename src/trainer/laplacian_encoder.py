e8ae4d96b1330d7bb71919e32eef072308a509b1
Mohammad Sami Nur Islam <saminur@narval2.narval.calcul.quebec>
2025-04-13 02:33:43 -0400

    Higher ent coeff

--- src/trainer/laplacian_encoder.py
+++ src/trainer/laplacian_encoder.py
@@ -67,7 +67,7 @@ class LaplacianEncoderTrainer(Trainer, ABC):
             lam=0.95,
             learning_rate=3e-4,
             clip_range=0.2,
-            ent_coef=0.05
+            ent_coef=0.2
             # etc...
         )
 
